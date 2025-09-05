5529c55958c3b2ad3d43005a3c8d371309bdf9cd
Egor <bananaxvii812@gmail.com>
2025-09-05 19:25:40 +0500

    Add function which return class for depends

--- app/depends.py
+++ app/depends.py
@@ -1,4 +1,12 @@
 from app.repositories.database import db
+from app.services.url_checks import URLInspector
+
+inspector: URLInspector | None = None
+
+async def get_inspector() -> URLInspector:
+    if inspector is None:
+        raise RuntimeError("Inspector is not initialized")
+    return inspector
 
 async def get_links_collection():
     return db["links"]
