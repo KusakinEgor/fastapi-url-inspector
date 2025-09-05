66ff333923b645badf0490e6fbab8196c79a419e
Egor <bananaxvii812@gmail.com>
2025-09-05 19:27:52 +0500

    Add route

--- app/routing/analyze.py
+++ app/routing/analyze.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.analyze import UrlRequest, UrlResponse
+from app.depends import get_inspector
+from app.services.url_checks import URLInspector
 
-router = APIRouter(prefix="/analyze", tags=["Analyze"])
+router = APIRouter(tags=["Analyze"])
 
-@router.post("", response_model=UrlResponse)
-async def inspect_url(request: UrlRequest):
-    pass
+@router.post("/analyze", response_model=UrlResponse)
+async def inspect_url(
+    request: UrlRequest,
+    inspector: URLInspector = Depends(get_inspector)
+):
+    status_headers = await inspector.check_status(request.link)
+
+    if status_headers is not None:
+      status_code, headers = status_headers
+    else:
+       status_code, headers = None, None
+
+
+    response_time = await inspector.measure_response_time(request.link)
+    ssl_information = await inspector.check_ssl(request.link)
+
+    return UrlResponse(
+      status=status_code,
+      headers=headers,
+      response_time=response_time,
+      ssl=ssl_information is not None and ssl_information.get("valid"),
+      meta={"ssl_information": ssl_information},
+    )
