1b278fb35d31c8cd94e4151c19b6c8909ddd7e3c
Egor <bananaxvii812@gmail.com>
2025-09-18 15:31:26 +0500

    Back changes

--- app/repositories/redis_cache.py
+++ app/repositories/redis_cache.py
@@ -1,7 +1,7 @@
 import redis.asyncio as redis
 from app.config import REDIS_URL
 
-redis_client = redis.from_url(REDIS_URL, decode_responses=True)
+redis_client = redis.Redis(REDIS_URL, decode_responses=True)
 
 async def set_value(key: str, value: str):
     await redis_client.set(key, value, ex=60)
